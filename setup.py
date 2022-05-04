f23f0ded7fbb46d73a3f90aa8012d0e275c285c9
Ilyas Semmaoui <ilyas.semmaoui@budget-insight.com>
2022-05-04 19:37:49 +0200

    Update setup.py and requirements.txt

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open("README.md", "r") as fh:
 	long_description = fh.read()
 
 setup(name='gplaycli',
-		version='3.29',
+		version='3.30',
 		description='GPlayCli, a Google play downloader command line interface',
 		long_description=long_description,
 		long_description_content_type="text/markdown",
 		author="Matlink",
 		author_email="matlink@matlink.fr",
-		url="https://github.com/matlink/gplaycli",
+		url="https://github.com/Wadeih/gplaycli",
 		license="AGPLv3",
 		entry_points={
 			'console_scripts': [
@@ -29,7 +29,8 @@ setup(name='gplaycli',
 			['etc/gplaycli', ['gplaycli.conf']],
 		],
 		install_requires=[
-				'matlink-gpapi>=0.4.4.4',
+				'gpapi-auth-fixed @ git+ssh://git@github.com/Wadeih/googleplay-api@master#egg=gpapi-auth-fixed',
 				'pyaxmlparser',
 		],
+
 )
